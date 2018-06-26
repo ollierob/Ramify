84efa64d9c0edd67d747de09590d281a305eb398
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2018-06-26 14:05:00 +0100

    Records

--- data/church-records/src/main/java/net/ramify/model/record/church/ChurchRecord.java
+++ data/church-records/src/main/java/net/ramify/model/record/church/ChurchRecord.java
@@ -1,13 +1,17 @@
 package net.ramify.model.record.church;
 
 import net.ramify.model.place.address.Address;
+import net.ramify.model.place.address.HasAddress;
 import net.ramify.model.record.Record;
 
 import javax.annotation.Nonnull;
 
-public interface ChurchRecord extends Record {
+public interface ChurchRecord extends Record, HasAddress {
 
+    /**
+     * @return the church address.
+     */
     @Nonnull
-    Address churchAddress();
+    Address address();
 
 }
