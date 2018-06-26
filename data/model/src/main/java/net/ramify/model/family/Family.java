a4d06dd0a9e21a23d72c77b154f38da57966a62e
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2018-06-26 15:37:37 +0100

    Family builder

--- data/model/src/main/java/net/ramify/model/family/Family.java
+++ data/model/src/main/java/net/ramify/model/family/Family.java
@@ -1,9 +1,14 @@
 package net.ramify.model.family;
 
+import net.ramify.model.family.relationship.MarriedCouple;
+import net.ramify.model.family.relationship.ParentChild;
 import net.ramify.model.family.relationship.Relationship;
+import net.ramify.model.family.relationship.UnknownRelationship;
 import net.ramify.model.person.Person;
+import net.ramify.model.person.gender.Gender;
 
 import javax.annotation.Nonnull;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -30,8 +35,47 @@ public interface Family {
         return of(Set.of(relationship));
     }
 
+    static Family of(final Relationship... relationships) {
+        return of(Set.of(relationships));
+    }
+
     static Family of(final Set<Relationship> relationships) {
         return () -> relationships;
+    }
+
+    static Builder startingWith(final Person person) {
+        return new Builder(person);
+    }
+
+    class Builder {
+
+        private final Person person;
+
+        private Builder(final Person person) {
+            this.person = person;
+        }
+
+        public Family self() {
+            return of(person);
+        }
+
+        public Family withParent(final Person parent) {
+            return Family.of(new ParentChild(parent, person));
+        }
+
+        public Family withParents(final Person firstParent, final Person secondParent, final boolean spouses) {
+            final Set<Relationship> relationships = new HashSet<>();
+            relationships.add(new ParentChild(firstParent, person));
+            relationships.add(new ParentChild(secondParent, person));
+            relationships.add(spouses ? new MarriedCouple(firstParent, secondParent) : new UnknownRelationship(firstParent, secondParent));
+            return Family.of(relationships);
+        }
+
+        public Family withGrandparent(final Person grandparent) {
+            final Person unknownParent = Person.unknown(Gender.UNKNOWN);
+            return Family.of(new ParentChild(grandparent, unknownParent), new ParentChild(unknownParent, person));
+        }
+
     }
 
 }
