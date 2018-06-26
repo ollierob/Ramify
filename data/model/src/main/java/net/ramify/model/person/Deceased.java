96bcb9af1210b37cabb20ef4116f556b557aa06d
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2018-06-26 21:04:32 +0100

    Deceased

--- data/model/src/main/java/net/ramify/model/person/Deceased.java
+++ data/model/src/main/java/net/ramify/model/person/Deceased.java
@@ -3,54 +3,19 @@ package net.ramify.model.person;
 import net.ramify.model.date.DateRange;
 import net.ramify.model.event.Death;
 import net.ramify.model.event.PersonalEvents;
-import net.ramify.model.person.age.AgeRange;
-import net.ramify.model.person.gender.Gender;
-import net.ramify.model.person.name.Name;
 
 import javax.annotation.Nonnull;
 
-public class Deceased implements PersonalAttributes {
-
-    private final PersonalAttributes delegate;
+public class Deceased extends DelegatedPersonalAttributes {
 
     public Deceased(final PersonalAttributes delegate) {
-        this.delegate = delegate;
-    }
-
-    @Nonnull
-    @Override
-    public Person person() {
-        return delegate.person();
-    }
-
-    @Nonnull
-    @Override
-    public DateRange date() {
-        return delegate.date();
-    }
-
-    @Nonnull
-    @Override
-    public Name name() {
-        return delegate.name();
-    }
-
-    @Nonnull
-    @Override
-    public AgeRange age() {
-        return delegate.age();
-    }
-
-    @Nonnull
-    @Override
-    public Gender gender() {
-        return delegate.gender();
+        super(delegate);
     }
 
     @Nonnull
     @Override
     public PersonalEvents inferredEvents() {
-        return delegate.inferredEvents().and(new Death(DateRange.before(this.date())));
+        return super.inferredEvents().and(new Death(DateRange.before(this.date())));
     }
 
 }
---
+++ data/model/src/main/java/net/ramify/model/person/DelegatedPersonalAttributes.java
@@ -0,0 +1,54 @@
+package net.ramify.model.person;
+
+import net.ramify.model.date.DateRange;
+import net.ramify.model.event.PersonalEvents;
+import net.ramify.model.person.age.AgeRange;
+import net.ramify.model.person.gender.Gender;
+import net.ramify.model.person.name.Name;
+
+import javax.annotation.Nonnull;
+
+public abstract class DelegatedPersonalAttributes implements PersonalAttributes {
+
+    private final PersonalAttributes delegate;
+
+    protected DelegatedPersonalAttributes(final PersonalAttributes delegate) {
+        this.delegate = delegate;
+    }
+
+    @Override
+    @Nonnull
+    public Person person() {
+        return delegate.person();
+    }
+
+    @Override
+    @Nonnull
+    public DateRange date() {
+        return delegate.date();
+    }
+
+    @Override
+    @Nonnull
+    public Name name() {
+        return delegate.name();
+    }
+
+    @Override
+    @Nonnull
+    public AgeRange age() {
+        return delegate.age();
+    }
+
+    @Override
+    @Nonnull
+    public Gender gender() {
+        return delegate.gender();
+    }
+
+    @Override
+    @Nonnull
+    public PersonalEvents inferredEvents() {
+        return delegate.inferredEvents();
+    }
+}
