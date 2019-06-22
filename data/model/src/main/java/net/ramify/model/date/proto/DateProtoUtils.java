f0245c7827aa7390d1fa49d8d72636f7e5586207
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2019-06-22 13:15:00 +0100

    Protos

--- data/model/src/main/java/net/ramify/model/date/proto/DateProtoUtils.java
+++ data/model/src/main/java/net/ramify/model/date/proto/DateProtoUtils.java
@@ -1,11 +1,24 @@
 package net.ramify.model.date.proto;
 
+import javax.annotation.Nonnull;
+import java.time.LocalDate;
 import java.time.chrono.ChronoLocalDate;
 
 public class DateProtoUtils {
 
+    @Nonnull
     public static DateProto.Date toProto(final ChronoLocalDate date) {
+        if (date instanceof LocalDate) return toProto((LocalDate) date);
         throw new UnsupportedOperationException();
+    }
+
+    @Nonnull
+    public static DateProto.Date toProto(final LocalDate date) {
+        return DateProto.Date.newBuilder()
+                .setYear(date.getYear())
+                .setMonth(date.getMonthValue())
+                .setDay(date.getDayOfMonth())
+                .build();
     }
 
 }
