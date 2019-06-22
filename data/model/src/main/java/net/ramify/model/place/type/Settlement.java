39b4950ffb2f692c1bd9e762bc241a2297908fbe
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2019-06-22 10:12:57 +0100

    Places

--- data/model/src/main/java/net/ramify/model/place/type/Settlement.java
+++ data/model/src/main/java/net/ramify/model/place/type/Settlement.java
@@ -1,6 +1,13 @@
 package net.ramify.model.place.type;
 
+import javax.annotation.Nonnull;
+
 public interface Settlement extends SettlementOrRegion {
+
+    @Nonnull
+    default Region parent() {
+        return this.region();
+    }
 
     @Override
     default <R> R handleWith(final PlaceHandler<R> handler) {
