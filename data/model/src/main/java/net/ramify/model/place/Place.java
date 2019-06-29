793ad9a03bec6fd8523cac7fc2bda0f665208f38
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2019-06-29 12:47:31 +0100

    Place proto

--- data/model/src/main/java/net/ramify/model/place/Place.java
+++ data/model/src/main/java/net/ramify/model/place/Place.java
@@ -4,6 +4,7 @@ import net.ramify.data.proto.BuildsProto;
 import net.ramify.model.place.proto.PlaceProto;
 import net.ramify.model.place.type.PlaceHandler;
 import net.ramify.utils.objects.Castable;
+import net.ramify.utils.objects.Consumers;
 
 import javax.annotation.CheckForNull;
 import javax.annotation.Nonnull;
@@ -41,9 +42,16 @@ public interface Place extends HasPlaceId, Castable<Place>, BuildsProto<PlacePro
     <R> R handleWith(PlaceHandler<R> handler);
 
     default PlaceProto.Place.Builder toProtoBuilder() {
-        return PlaceProto.Place.newBuilder()
+        final var builder = PlaceProto.Place.newBuilder()
+                .setId(this.placeId().value())
                 .setName(this.name())
-                .setAddress(this.address());
+                .setType(this.type());
+        Consumers.ifNonNull(this.parent(), parent -> builder.setParent(parent.toProto()));
+        return builder;
+    }
+
+    default String type() {
+        return this.getClass().getSimpleName();
     }
 
     @Nonnull
