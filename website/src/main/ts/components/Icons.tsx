187340aa98e87e923aab4b25f662d912e12273f4
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2019-06-30 16:59:38 +0100

    Icons

---
+++ website/src/main/ts/components/Icons.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import {Icon} from "antd";
+
+type IconProps = {};
+
+export const TreeIcon = (props: IconProps) => <Icon type="apartment" {...props}/>
+export const PeopleIcon = (props: IconProps) => <Icon type="user" {...props}/>
+export const PlacesIcon = (props: IconProps) => <Icon type="global" {...props}/>
--- website/src/main/ts/pages/HeaderMenu.tsx
+++ website/src/main/ts/pages/HeaderMenu.tsx
@@ -1,5 +1,6 @@
 import * as React from "react";
 import {Menu} from "antd";
+import {PeopleIcon, PlacesIcon, TreeIcon} from "../components/Icons";
 
 type Props = {
     active: string;
@@ -9,9 +10,9 @@ export default class HeaderMenu extends React.PureComponent<Props> {
 
     render() {
         return <Menu mode="horizontal" selectedKeys={[this.props.active]} className="menu">
-            <Menu.Item key="trees">Trees</Menu.Item>
-            <Menu.Item key="people">People</Menu.Item>
-            <Menu.Item key="places">Places</Menu.Item>
+            <Menu.Item key="trees"><TreeIcon/> Trees</Menu.Item>
+            <Menu.Item key="people"><PeopleIcon/> People</Menu.Item>
+            <Menu.Item key="places"><PlacesIcon/> Places</Menu.Item>
         </Menu>;
     }
 
