d535e65ee9476cc5df7c46c00ee290be4e82c6ca
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2019-07-03 20:19:52 +0100

    Resources

--- server/rest/src/main/java/net/ramify/server/resource/places/InstitutionResource.java
+++ server/rest/src/main/java/net/ramify/server/resource/places/InstitutionResource.java
@@ -1,7 +1,9 @@
 package net.ramify.server.resource.places;
 
 import net.ramify.model.place.PlaceId;
-import net.ramify.model.place.institution.church.ChurchInfo;
+import net.ramify.model.place.collection.PlaceIds;
+import net.ramify.model.place.institution.InstitutionInfo;
+import net.ramify.server.resource.Resource;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -12,11 +14,14 @@ import javax.ws.rs.core.MediaType;
 import static net.ramify.server.resource.Resource.APPLICATION_PROTOBUF;
 
 @Produces({MediaType.TEXT_PLAIN, APPLICATION_PROTOBUF})
-public interface ChurchesResource extends InstitutionResource {
+public interface InstitutionResource extends Resource {
 
-    @Override
     @GET
     @Path("at/{id}")
-    ChurchInfo info(@PathParam("id") PlaceId churchId);
+    InstitutionInfo info(@PathParam("id") PlaceId churchId);
+
+    @GET
+    @Path("in/{region}")
+    PlaceIds inRegion(@PathParam("region") PlaceId region);
 
 }
