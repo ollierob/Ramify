00da64b1070c0796350fb390b154002f8657cab9
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2019-07-08 20:08:27 +0100

    Record sets

--- data/model/src/main/java/net/ramify/model/record/collection/RecordSet.java
+++ data/model/src/main/java/net/ramify/model/record/collection/RecordSet.java
@@ -1,12 +1,15 @@
 package net.ramify.model.record.collection;
 
 import net.ramify.model.record.HasTitleDescription;
-import net.ramify.model.record.group.HasRecordSetGroupId;
-import net.ramify.model.record.group.RecordSetGroup;
+
+import javax.annotation.CheckForNull;
 
 /**
- * @see RecordSetGroup
+ *
  */
-public interface RecordSet extends HasTitleDescription, HasRecordSetId, HasRecordSetGroupId {
+public interface RecordSet extends HasTitleDescription, HasRecordSetId {
+
+    @CheckForNull
+    RecordSetId parentId();
 
 }
