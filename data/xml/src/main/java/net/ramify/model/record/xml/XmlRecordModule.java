478b0f883747e7aacd6a7c59d48f054880f04610
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2019-07-10 19:09:03 +0100

    XML records

---
+++ data/xml/src/main/java/net/ramify/model/record/xml/XmlRecordModule.java
@@ -0,0 +1,47 @@
+package net.ramify.model.record.xml;
+
+import com.google.common.base.Preconditions;
+import com.google.inject.PrivateModule;
+import com.google.inject.Provides;
+import com.google.inject.Singleton;
+import net.ramify.model.place.xml.XmlPlaceModule;
+import net.ramify.model.record.provider.RecordSetProvider;
+import net.ramify.model.record.xml.collection.XmlRecordSets;
+
+import javax.annotation.Nonnull;
+import javax.inject.Named;
+import javax.xml.bind.JAXBContext;
+import javax.xml.bind.JAXBException;
+import java.io.File;
+import java.net.URISyntaxException;
+
+public class XmlRecordModule extends PrivateModule {
+
+    @Override
+    protected void configure() {
+        this.expose(RecordSetProvider.class);
+    }
+
+    @Provides
+    @Singleton
+    JAXBContext provideContext() throws JAXBException {
+        return JAXBContext.newInstance(XmlRecordSets.class);
+    }
+
+    @Provides
+    @Nonnull
+    @Singleton
+    @Named("data")
+    File provideXmlDirectory() throws URISyntaxException {
+        final var places = XmlPlaceModule.class.getResource("/xml/data/records");
+        Preconditions.checkState(places != null, "Could not load XML data");
+        return new File(places.toURI());
+    }
+
+    @Provides
+    @Singleton
+    RecordSetProvider provideRecordSetProvider(final JAXBContext context) {
+        throw new UnsupportedOperationException();
+    }
+
+}
