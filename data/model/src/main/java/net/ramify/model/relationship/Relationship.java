a1760a4f2ea88f8d658d5d81783562beec0219cd
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2019-07-11 21:36:00 +0100

    Protos

--- data/model/src/main/java/net/ramify/model/relationship/Relationship.java
+++ data/model/src/main/java/net/ramify/model/relationship/Relationship.java
@@ -1,12 +1,14 @@
 package net.ramify.model.relationship;
 
+import net.ramify.data.proto.BuildsProto;
 import net.ramify.model.person.PersonId;
+import net.ramify.model.relationship.proto.RelationshipProto;
 import net.ramify.model.relationship.type.RelationshipHandler;
 import net.ramify.utils.objects.Castable;
 
 import javax.annotation.Nonnull;
 
-public interface Relationship extends Castable<Relationship> {
+public interface Relationship extends Castable<Relationship>, BuildsProto<RelationshipProto.Relationship> {
 
     @Nonnull
     PersonId fromId();
@@ -14,17 +16,31 @@ public interface Relationship extends Castable<Relationship> {
     @Nonnull
     PersonId toId();
 
-    default boolean has(final PersonId personId) {
+    default boolean has(@Nonnull final PersonId personId) {
         return personId.equals(this.fromId()) || personId.equals(this.toId());
     }
 
-    <R> R handleWith(final RelationshipHandler<R> handler);
-
     boolean isDirected();
+
+    <R> R handleWith(final RelationshipHandler<R> handler);
 
     @Nonnull
     default Relationship inverse() {
         return new InverseRelationship<>(this);
+    }
+
+    @Nonnull
+    default RelationshipProto.Relationship.Builder toProtoBuilder() {
+        return RelationshipProto.Relationship.newBuilder()
+                .setFromId(this.fromId().value())
+                .setToId(this.toId().value())
+                .setDirected(this.isDirected());
+    }
+
+    @Nonnull
+    @Override
+    default RelationshipProto.Relationship toProto() {
+        return this.toProtoBuilder().build();
     }
 
 }
