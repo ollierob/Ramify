576f99b6dd4567a83c01418c8431a0534df07c12
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2019-07-11 09:28:59 +0100

    Records

--- data/xml/src/main/java/net/ramify/model/record/xml/collection/XmlRecordSetType.java
+++ data/xml/src/main/java/net/ramify/model/record/xml/collection/XmlRecordSetType.java
@@ -10,17 +10,39 @@ import javax.xml.bind.annotation.XmlType;
 @XmlType(namespace = XmlRecord.NAMESPACE)
 enum XmlRecordSetType {
 
+    @XmlEnumValue("birth")
+    BIRTH,
+
+    @XmlEnumValue("baptism")
+    BAPTISM,
+
+    @XmlEnumValue("marriage")
+    MARRIAGE,
+
+    @XmlEnumValue("census")
+    CENSUS(RecordProto.RecordType.RESIDENCE),
+
+    @XmlEnumValue("death")
+    DEATH,
+
+    @XmlEnumValue("burial")
+    BURIAL,
+
     @XmlEnumValue("mixed")
-    MIXED(RecordProto.RecordType.MIXED);
+    MIXED;
 
     private final RecordProto.RecordType type;
+
+    XmlRecordSetType() {
+        this(null);
+    }
 
     XmlRecordSetType(final RecordProto.RecordType type) {
         this.type = type;
     }
 
     RecordProto.RecordType type() {
-        return type;
+        return type == null ? RecordProto.RecordType.valueOf(this.name()) : type;
     }
 
 }
