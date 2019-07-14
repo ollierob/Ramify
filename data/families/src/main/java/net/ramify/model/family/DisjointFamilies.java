077f8557372b0daacd3045262a8c9f3d794adac2
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2019-07-14 16:29:02 +0100

    Families

---
+++ data/families/src/main/java/net/ramify/model/family/DisjointFamilies.java
@@ -0,0 +1,39 @@
+package net.ramify.model.family;
+
+import net.ramify.model.person.Person;
+import net.ramify.model.person.PersonId;
+import net.ramify.model.relationship.Relationship;
+
+import javax.annotation.Nonnull;
+import java.util.Collection;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class DisjointFamilies implements Family {
+
+    private final Set<Family> families;
+
+    public DisjointFamilies(final Set<Family> families) {
+        this.families = families;
+    }
+
+    @Nonnull
+    @Override
+    public Set<? extends Person> people() {
+        return families.stream().map(Family::people).flatMap(Collection::stream).collect(Collectors.toSet());
+    }
+
+    @Nonnull
+    @Override
+    public Set<? extends Relationship> relationships() {
+        return families.stream().map(Family::relationships).flatMap(Collection::stream).collect(Collectors.toSet());
+    }
+
+    @Nonnull
+    @Override
+    public Optional<Relationship> between(final PersonId from, final PersonId to) {
+        return families.stream().map(f -> f.between(from, to).orElse(null)).findAny();
+    }
+
+}
