ce32e31801f253796d0c5fb0514c674f7d2d3b34
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2019-10-24 10:50:33 +0100

    Place groups

--- data/model/src/main/java/net/ramify/model/place/PlaceGroup.java
+++ data/model/src/main/java/net/ramify/model/place/PlaceGroup.java
@@ -1,12 +1,15 @@
 package net.ramify.model.place;
 
+import com.google.common.collect.Iterables;
+import net.ramify.data.proto.BuildsProto;
+import net.ramify.model.place.proto.PlaceProto;
 import net.ramify.model.place.provider.PlaceProvider;
 import net.ramify.utils.collections.SetUtils;
 
 import javax.annotation.Nonnull;
 import java.util.Set;
 
-public interface PlaceGroup {
+public interface PlaceGroup extends BuildsProto<PlaceProto.PlaceGroup> {
 
     @Nonnull
     PlaceGroupId id();
@@ -23,6 +26,21 @@ public interface PlaceGroup {
     @Nonnull
     default Set<Place> children(final PlaceProvider places) {
         return SetUtils.transformIgnoreNull(this.childIds(), places::require);
+    }
+
+    @Nonnull
+    default PlaceProto.PlaceGroup.Builder toProtoBuilder() {
+        return PlaceProto.PlaceGroup.newBuilder()
+                .setId(this.id().value())
+                .setName(this.name())
+                .setDefaultChildId(this.defaultChildId().value())
+                .addAllOtherChildId(Iterables.transform(this.childIds(), PlaceId::value));
+    }
+
+    @Nonnull
+    @Override
+    default PlaceProto.PlaceGroup toProto() {
+        return this.toProtoBuilder().build();
     }
 
 }
