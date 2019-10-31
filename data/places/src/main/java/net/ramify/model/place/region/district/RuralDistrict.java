cf9cf0d00eff772aaeb7f8fd92f503887307ac7d
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2019-10-31 20:43:24 +0000

    Data

--- data/places/src/main/java/net/ramify/model/place/region/district/RuralDistrict.java
+++ data/places/src/main/java/net/ramify/model/place/region/district/RuralDistrict.java
@@ -10,11 +10,19 @@ public class RuralDistrict extends AbstractRegion implements District {
 
     private final County parent;
 
-    public RuralDistrict(final PlaceId id, final String name, final Place parent, final PlaceGroupId groupId) throws InvalidPlaceTypeException {
+    public RuralDistrict(
+            final PlaceId id,
+            final String name,
+            final Place parent,
+            final PlaceGroupId groupId) throws InvalidPlaceTypeException {
         this(id, name, parent.requireAs(County.class), groupId);
     }
 
-    public RuralDistrict(final PlaceId id, final String name, final County parent, final PlaceGroupId groupId) {
+    public RuralDistrict(
+            final PlaceId id,
+            final String name,
+            final County parent,
+            final PlaceGroupId groupId) {
         super(id, name, groupId);
         this.parent = parent;
     }
