5df434aa8488be23ba34dc6b1ff256ef95514c51
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2019-11-01 22:08:49 +0000

    Borough history

--- data/places/src/main/java/net/ramify/model/place/region/district/MetropolitanBorough.java
+++ data/places/src/main/java/net/ramify/model/place/region/district/MetropolitanBorough.java
@@ -1,8 +1,11 @@
 package net.ramify.model.place.region.district;
 
 import com.google.common.collect.ImmutableSet;
+import net.ramify.model.date.ExactDate;
+import net.ramify.model.place.DefaultPlaceHistory;
 import net.ramify.model.place.Place;
 import net.ramify.model.place.PlaceGroupId;
+import net.ramify.model.place.PlaceHistory;
 import net.ramify.model.place.PlaceId;
 import net.ramify.model.place.proto.PlaceProto;
 import net.ramify.model.place.region.AbstractRegion;
@@ -10,11 +13,13 @@ import net.ramify.model.place.type.Region;
 import net.ramify.model.place.type.Settlement;
 
 import javax.annotation.Nonnull;
+import java.time.Month;
 import java.util.Set;
 
 public class MetropolitanBorough extends AbstractRegion implements District {
 
     private static final Set<Class<? extends Place>> CHILD_TYPES = ImmutableSet.of(Region.class, Settlement.class);
+    private static final PlaceHistory BOROUGH_HISTORY = new DefaultPlaceHistory(ExactDate.on(1974, Month.APRIL, 1), null);
 
     private final Region parent;
 
@@ -23,7 +28,7 @@ public class MetropolitanBorough extends AbstractRegion implements District {
     }
 
     public MetropolitanBorough(final PlaceId id, final String name, final Region parent, final PlaceGroupId groupId) {
-        super(id, name, groupId);
+        super(id, name, groupId, BOROUGH_HISTORY);
         this.parent = parent;
     }
 
