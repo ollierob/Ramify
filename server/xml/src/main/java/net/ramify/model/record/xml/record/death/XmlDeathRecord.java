0062c991c671d0c2bb4ed9d497e920d8bc03cc8a
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2019-11-02 11:48:02 +0000

    MI records

--- server/xml/src/main/java/net/ramify/model/record/xml/record/death/XmlDeathRecord.java
+++ server/xml/src/main/java/net/ramify/model/record/xml/record/death/XmlDeathRecord.java
@@ -6,7 +6,11 @@ import net.ramify.model.date.xml.XmlDateRange;
 import net.ramify.model.date.xml.XmlExactDate;
 import net.ramify.model.event.Event;
 import net.ramify.model.event.type.death.GenericDeath;
+import net.ramify.model.family.Family;
+import net.ramify.model.family.FamilyBuilder;
 import net.ramify.model.family.SinglePersonFamily;
+import net.ramify.model.family.xml.XmlRelationship;
+import net.ramify.model.person.Person;
 import net.ramify.model.person.PersonId;
 import net.ramify.model.place.PlaceId;
 import net.ramify.model.record.civil.GenericDeathRecord;
@@ -17,9 +21,12 @@ import net.ramify.model.record.xml.record.XmlPersonOnDateRecord;
 import net.ramify.model.record.xml.record.XmlRecord;
 import net.ramify.utils.objects.Functions;
 
+import javax.annotation.Nonnull;
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlElementRef;
 import javax.xml.bind.annotation.XmlRootElement;
+import java.util.List;
 import java.util.Set;
 
 @XmlRootElement(namespace = XmlRecord.NAMESPACE, name = "death")
@@ -31,6 +38,10 @@ public class XmlDeathRecord extends XmlPersonOnDateRecord {
     @XmlAttribute(name = "deathPlace", namespace = XmlRecord.NAMESPACE)
     private String deathPlace;
 
+    @XmlElementRef
+    private List<XmlRelationship> relationships;
+
+    @Nonnull
     public DeathRecord build(final PlaceId recordSetCovers, final RecordContext context, final RecordSet recordSet) {
         final var date = this.deathDate();
         final var person = this.person(date, context);
@@ -40,7 +51,15 @@ public class XmlDeathRecord extends XmlPersonOnDateRecord {
                 recordSet,
                 date,
                 deathPlace,
-                new SinglePersonFamily(person));
+                this.family(person, context, date));
+    }
+
+    Family family(final Person person, final RecordContext context, final ExactDate date) {
+        if (relationships == null || relationships.isEmpty()) return new SinglePersonFamily(person);
+        final var builder = new FamilyBuilder();
+        builder.addPerson(person);
+        relationships.forEach(relationship -> relationship.addRelationship(person, builder, context.nameParser(), date));
+        return builder.build();
     }
 
     @Override
