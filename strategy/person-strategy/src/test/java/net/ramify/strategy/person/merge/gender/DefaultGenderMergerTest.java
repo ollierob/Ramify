7dba2525a22e66d1436b500197836701585d32ca
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2019-11-23 11:55:49 +0000

    Merging

---
+++ strategy/person-strategy/src/test/java/net/ramify/strategy/person/merge/gender/DefaultGenderMergerTest.java
@@ -0,0 +1,46 @@
+package net.ramify.strategy.person.merge.gender;
+
+import net.ramify.model.person.gender.Gender;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class DefaultGenderMergerTest {
+
+    private DefaultGenderMerger testMerger;
+
+    @BeforeEach
+    void beforeEach() {
+        testMerger = new DefaultGenderMerger();
+    }
+
+    @Test
+    void testMatcher() {
+
+        {
+            final var result = testMerger.merge(Gender.UNKNOWN, Gender.UNKNOWN);
+            assertEquals(Optional.of(Gender.UNKNOWN), result.value());
+        }
+
+        {
+            final var result = testMerger.merge(Gender.MALE, Gender.UNKNOWN);
+            assertEquals(Optional.of(Gender.MALE), result.value());
+        }
+
+        {
+            final var result = testMerger.merge(Gender.UNKNOWN, Gender.MALE);
+            assertEquals(Optional.of(Gender.MALE), result.value());
+        }
+
+        {
+            final var result = testMerger.merge(Gender.FEMALE, Gender.MALE);
+            assertTrue(result.isImpossible());
+        }
+
+    }
+
+}
