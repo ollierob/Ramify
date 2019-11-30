b04a96f5a043f80d174889181fcf5a0bd9c04658
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2019-11-30 21:25:48 +0000

    Markdown

--- website/src/components/layout/Markdown.tsx
+++ website/src/components/layout/Markdown.tsx
@@ -1,15 +1,31 @@
 import * as React from "react";
 import {HasClass} from "../style/HasClass";
 
+type State = {text: string, parsed: React.ReactNode}
+
 export const Markdown = (props: {text: string} & HasClass) => {
+
+    const [state, setState] = React.useState<State>();
+
     const text = props.text;
     if (!text) return null;
-    const paragraphs = text.split("\n\n");
-    return <div {...props}>
-        {paragraphs.length > 1 ? paragraphs.map(p => <p>{format(p)}</p>) : format(text)}
-    </div>;
+
+    let parsed = state ? state.parsed : null;
+    if (!state || state.text != text) {
+        parsed = parse(text);
+        setState({text, parsed});
+    }
+
+    return <div {...props}>{parsed}</div>;
+
 };
 
-function format(s: string): React.ReactNode {
+function parse(s: string): React.ReactNode {
+    const split = s.split("\n\n");
+    if (split.length > 1) return <>{split.map(s => <p>{formatParagraph(s)}</p>)}</>;
+    return s;
+}
+
+function formatParagraph(s: string): React.ReactNode {
     return s;
 }
