2550bc414387ebaee89c5843702032dff77acb58
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2019-12-01 16:18:16 +0000

    Markdown

--- website/src/components/layout/Markdown.tsx
+++ website/src/components/layout/Markdown.tsx
@@ -1,22 +1,26 @@
 import * as React from "react";
 import {HasClass} from "../style/HasClass";
 
+const ReactMarkdown = require('react-markdown');
+
 type State = {text: string, parsed: React.ReactNode}
 
 export const Markdown = (props: {text: string} & HasClass) => {
 
-    const [state, setState] = React.useState<State>();
-
+    // const [state, setState] = React.useState<State>();
+    //
     const text = props.text;
     if (!text) return null;
+    //
+    // let parsed = state ? state.parsed : null;
+    // if (!state || state.text != text) {
+    //     parsed = parse(text);
+    //     setState({text, parsed});
+    // }
+    //
+    // return <div {...props}>{parsed}</div>;
 
-    let parsed = state ? state.parsed : null;
-    if (!state || state.text != text) {
-        parsed = parse(text);
-        setState({text, parsed});
-    }
-
-    return <div {...props}>{parsed}</div>;
+    return <ReactMarkdown source={text}/>;
 
 };
 
