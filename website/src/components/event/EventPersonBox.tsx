4934fcb005f405fb6215d477cb49e787b53b04fb
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2019-12-10 21:20:10 +0000

    Event box

--- website/src/components/event/EventPersonBox.tsx
+++ website/src/components/event/EventPersonBox.tsx
@@ -5,11 +5,13 @@ import {personProfileHref} from "../../pages/people/PeopleLinks";
 import {PersonName} from "../people/PersonName";
 import {EmptyLifespanWords, LifespanWords, renderLifespan} from "../people/Lifespan";
 
-const Words: LifespanWords = {...EmptyLifespanWords, born: "Born ", died: "Died "};
+const Words: LifespanWords = {...EmptyLifespanWords, born: "born ", died: "died "};
 
 export const EventPersonBox = (props: {prefix: string, person: Person.AsObject, tree: FamilyTreeId}) => {
 
     if (!props.person) return null;
+
+    const lifespan = renderLifespan(props.person, Words);
 
     return <div className="person bordered">
         {props.prefix}
@@ -17,7 +19,7 @@ export const EventPersonBox = (props: {prefix: string, person: Person.AsObject, 
         <a href={personProfileHref(props.tree, props.person.id)}>
             <PersonName name={props.person.name}/>
         </a>
-        {renderLifespan(props.person, Words)}
+        {lifespan && <span className="life">({lifespan})</span>}
     </div>;
 
 };
