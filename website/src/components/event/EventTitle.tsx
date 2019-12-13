4acca2d47dd700c98e02f6e02feb8b6cd98f96c0
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2019-12-13 18:04:21 +0000

    Data

--- website/src/components/event/EventTitle.tsx
+++ website/src/components/event/EventTitle.tsx
@@ -5,17 +5,21 @@ import {PersonName} from "../people/PersonName";
 import {Person} from "../../protobuf/generated/person_pb";
 import {RelativeRelationship} from "../relationship/RelativeRelationship";
 import {isFemale, isMale} from "../people/Gender";
+import {FamilyTreeId} from "../tree/FamilyTree";
+import {personProfileHref} from "../../pages/people/PeopleLinks";
 
 type Props = {
     event: Event.AsObject
     person: Person.AsObject
     relationship?: RelativeRelationship
+    tree: FamilyTreeId
 }
 
 export const EventTitle = (props: Props) => {
     return <>
         {props.event.title}
-        {props.relationship && props.relationship != "self" && <> of {renderRelationship(props.relationship, props.person)} <PersonName name={props.person.name}/></>}
+        {props.relationship && props.relationship != "self" && <> of {renderRelationship(props.relationship, props.person)} <a href={personProfileHref(props.tree, props.person.id)}><PersonName
+            name={props.person.name}/></a></>}
     </>;
 };
 
