118c036b7b1520e059d4ec22729f3ae675e52581
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2019-12-22 22:05:32 +0000

    Data

--- data/model/src/main/java/net/ramify/model/place/PlaceGroupId.java
+++ data/model/src/main/java/net/ramify/model/place/PlaceGroupId.java
@@ -7,6 +7,8 @@ import net.ramify.model.place.iso.CountryIso;
 import javax.annotation.Nonnull;
 import java.util.Objects;
 
+import static net.ramify.utils.StringUtils.isBlank;
+
 public class PlaceGroupId extends Id {
 
     private CountryIso iso;
@@ -37,6 +39,7 @@ public class PlaceGroupId extends Id {
     }
 
     public PlaceId withPlaceType(final String type) {
+        Preconditions.checkArgument(!isBlank(type), "Invalid (empty) type");
         return new PlaceId(this.value() + ':' + type);
     }
 
--- server/xml/src/main/java/net/ramify/model/place/xml/place/group/XmlPlaceGroup.java
+++ server/xml/src/main/java/net/ramify/model/place/xml/place/group/XmlPlaceGroup.java
@@ -1,13 +1,17 @@
 package net.ramify.model.place.xml.place.group;
 
+import net.ramify.model.place.Place;
 import net.ramify.model.place.PlaceGroup;
 import net.ramify.model.place.PlaceGroupId;
+import net.ramify.model.place.PlaceId;
 import net.ramify.model.place.group.DefaultPlaceGroup;
 import net.ramify.model.place.provider.PlaceProvider;
 import net.ramify.model.place.xml.place.XmlPlace;
 
 import javax.xml.bind.annotation.XmlAttribute;
 import javax.xml.bind.annotation.XmlRootElement;
+
+import static net.ramify.utils.StringUtils.isBlank;
 
 @XmlRootElement(namespace = XmlPlace.NAMESPACE, name = "placeGroup")
 public class XmlPlaceGroup {
@@ -18,16 +22,23 @@ public class XmlPlaceGroup {
     @XmlAttribute(name = "name", required = true)
     private String name;
 
-    @XmlAttribute(name = "defaultChildType", required = true)
+    @XmlAttribute(name = "defaultChildType")
     private String defaultChildType;
+
+    @XmlAttribute(name = "defaultChildId")
+    private String defaultChildId;
 
     PlaceGroup toPlaceGroup(final PlaceProvider placeProvider) {
         final var id = this.placeGroupId();
         return new DefaultPlaceGroup(
                 id,
                 name,
-                placeProvider.require(id.withPlaceType(defaultChildType)),
+                this.defaultChild(placeProvider, id),
                 placeProvider.findByGroup(id));
+    }
+
+    protected Place defaultChild(final PlaceProvider placeProvider, final PlaceGroupId id) {
+        return placeProvider.require(isBlank(defaultChildId) ? id.withPlaceType(defaultChildType) : new PlaceId(defaultChildId));
     }
 
     protected PlaceGroupId placeGroupId() {
