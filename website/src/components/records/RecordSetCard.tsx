93752c40285300bd870eb90ea6ef018f20955cd6
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2019-12-26 11:51:50 +0000

    Extract record set card

---
+++ website/src/components/records/RecordSetCard.tsx
@@ -0,0 +1,38 @@
+import {ExternalRecordReference, RecordSet} from "../../protobuf/generated/record_pb";
+import {Card} from "antd";
+import {recordSetHref} from "../../pages/records/RecordLinks";
+import {formatYearRange} from "../date/DateFormat";
+import * as React from "react";
+import {Link} from "../style/Links";
+import {RecordSetTitling} from "./RecordSetCards";
+import {recordTypeName} from "./RecordType";
+
+export const RecordSetCard = (props: {record: RecordSet.AsObject} & RecordSetTitling) => {
+    const record = props.record;
+    let title = props.shortTitle ? shortTitle(record) : record.longtitle;
+    if (props.removePrefix && title.startsWith(props.removePrefix)) title = title.substring(props.removePrefix.length).trimLeft();
+    return <Card
+        title={<a href={recordSetHref(record)}>{title}</a>}
+        className="recordCard">
+        Available {formatYearRange(record.date)}
+        <br/>
+        {record.numrecords.toLocaleString()} records, {record.numindividuals.toLocaleString()} individuals
+        {record.externalreferenceList.map(ref => <RecordReference key={ref.reference} reference={ref}/>)}
+        {record.description && <div className="unimportant">{record.description}</div>}
+    </Card>;
+};
+
+const RecordReference = (props: {reference: ExternalRecordReference.AsObject}) => {
+    return <div className="reference">
+        <Link link={props.reference.link} iconPath={props.reference.archive.icon} newWindow>
+            {props.reference.reference}
+        </Link>
+    </div>;
+};
+
+function shortTitle(record: RecordSet.AsObject): string {
+    if (record.shorttitle) return record.shorttitle;
+    const name = recordTypeName(record);
+    if (name == "Mixed") return record.longtitle;
+    return name;
+}
--- website/src/components/records/RecordSetCards.tsx
+++ website/src/components/records/RecordSetCards.tsx
@@ -1,23 +1,20 @@
 import * as React from "react";
 import {Place} from "../../protobuf/generated/place_pb";
 import {Card} from "antd";
-import {ExternalRecordReference, RecordSet} from "../../protobuf/generated/record_pb";
+import {RecordSet} from "../../protobuf/generated/record_pb";
 import {placeHref} from "../../pages/places/PlaceLinks";
-import {recordSetHref} from "../../pages/records/RecordLinks";
 import "./RecordCards.css";
-import {recordTypeName} from "./RecordType";
 import {HasClass} from "../style/HasClass";
-import {Link} from "../style/Links";
 import {Loading} from "../style/Loading";
 import {sortRecordSetByTitle} from "./RecordSet";
-import {formatYearRange} from "../date/DateFormat";
+import {RecordSetCard} from "./RecordSetCard";
 
-type Titling = {
+export type RecordSetTitling = {
     shortTitle?: boolean;
     removePrefix?: string;
 }
 
-type Props = HasClass & Titling & {
+type Props = HasClass & RecordSetTitling & {
     loading?: boolean
     records: ReadonlyArray<RecordSet.AsObject>
     alsoSee?: ReadonlyArray<Place.AsObject>
@@ -40,7 +37,7 @@ export class RecordSetCards extends React.PureComponent<Props> {
 
             {this.props.loading && <Loading/>}
 
-            {records.map(record => <RecordCard {...this.props} key={record.id} record={record}/>)}
+            {records.map(record => <RecordSetCard {...this.props} key={record.id} record={record}/>)}
 
             {!this.props.loading && !records.length && !this.props.ignoreNone && <NoRecordCards/>}
 
@@ -51,29 +48,6 @@ export class RecordSetCards extends React.PureComponent<Props> {
     }
 
 }
-
-const RecordCard = (props: {record: RecordSet.AsObject} & Titling) => {
-    const record = props.record;
-    let title = props.shortTitle ? shortTitle(record) : record.longtitle;
-    if (props.removePrefix && title.startsWith(props.removePrefix)) title = title.substring(props.removePrefix.length).trimLeft();
-    return <Card
-        title={<a href={recordSetHref(record)}>{title}</a>}
-        className="recordCard">
-        Available {formatYearRange(record.date)}
-        <br/>
-        {record.numrecords.toLocaleString()} records, {record.numindividuals.toLocaleString()} individuals
-        {record.externalreferenceList.map(ref => <RecordReference key={ref.reference} reference={ref}/>)}
-        {record.description && <div className="unimportant">{record.description}</div>}
-    </Card>;
-};
-
-const RecordReference = (props: {reference: ExternalRecordReference.AsObject}) => {
-    return <div className="reference">
-        <Link link={props.reference.link} iconPath={props.reference.archive.icon} newWindow>
-            {props.reference.reference}
-        </Link>
-    </div>;
-};
 
 const AlsoSeeCard = (props: {alsoSee: ReadonlyArray<Place.AsObject>}) => {
     const alsoSee = props.alsoSee;
@@ -88,13 +62,6 @@ const AlsoSeeCard = (props: {alsoSee: ReadonlyArray<Place.AsObject>}) => {
         </ul>
     </Card>;
 };
-
-function shortTitle(record: RecordSet.AsObject): string {
-    if (record.shorttitle) return record.shorttitle;
-    const name = recordTypeName(record);
-    if (name == "Mixed") return record.longtitle;
-    return name;
-}
 
 export const NoRecordCards = () => {
     return <>No records known.</>;
