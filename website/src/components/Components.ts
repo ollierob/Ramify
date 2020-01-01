3ed0c39862dc6cc0b61f942e43c9e2def4351477
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2020-01-01 13:09:01 +0000

    Fix

--- website/src/components/Components.ts
+++ website/src/components/Components.ts
@@ -1,7 +1,14 @@
 import * as React from "react";
 
 export function joinComponents(nodes: ReadonlyArray<React.ReactNode>, join: React.ReactNode): React.ReactNode {
-    if (!nodes.length) return null;
-    if (nodes.length == 1) return nodes[0];
-    return nodes.filter(n => !!n).reduce((p, c) => [p, join, c]);
+    if (!nodes || !nodes.length) return null;
+    nodes = nodes.filter(n => !!n);
+    switch (nodes.length) {
+        case 0:
+            return null;
+        case 1:
+            return nodes[0];
+        default:
+            return nodes.reduce((p, c) => [p, join, c]);
+    }
 }
