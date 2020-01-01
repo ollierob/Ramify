7b43d8e1ede741a49c911fabce1d38161dffd716
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2020-01-01 20:43:05 +0000

    Fix probate

--- data/records/src/main/java/net/ramify/model/record/will/probate/ProbateRecord.java
+++ data/records/src/main/java/net/ramify/model/record/will/probate/ProbateRecord.java
@@ -4,13 +4,23 @@ import net.ramify.model.date.DateRange;
 import net.ramify.model.family.Family;
 import net.ramify.model.record.DateFamilyRecord;
 import net.ramify.model.record.RecordId;
+import net.ramify.model.record.RecordSetTypes;
 import net.ramify.model.record.collection.RecordSet;
+import net.ramify.model.record.collection.RecordSetType;
 import net.ramify.model.record.type.PostDeathRecord;
+
+import javax.annotation.Nonnull;
 
 public class ProbateRecord extends DateFamilyRecord implements PostDeathRecord {
 
     public ProbateRecord(final RecordId id, final RecordSet recordSet, final DateRange date, final Family family) {
         super(id, recordSet, date, family);
+    }
+
+    @Nonnull
+    @Override
+    public RecordSetType type() {
+        return RecordSetTypes.PROBATE;
     }
 
 }
