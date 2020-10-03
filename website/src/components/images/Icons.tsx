6012808ce2c41c80c026bc038e986c5d1b14954b
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2020-10-03 14:43:26 +0100

    Icons

--- website/src/components/images/Icons.tsx
+++ website/src/components/images/Icons.tsx
@@ -1,16 +1,21 @@
 import * as React from "react";
-import {Icon} from "antd";
 import {HasClass} from "../style/HasClass";
+import {Icon} from "antd";
 
 type IconProps = HasClass & {
+    title?: string,
     onClick?: React.MouseEventHandler<HTMLElement>
 };
 
 export const TreeIcon = (props: IconProps) => <Icon type="apartment" {...props}/>;
+export const AddTreeIcon = (props: IconProps) => <Icon type="file-add" {...props}/>;
+
 export const PersonIcon = (props: IconProps) => <Icon type="user" {...props}/>;
 export const PeopleIcon = (props: IconProps) => <TreeIcon {...props}/>;
+
 export const PlacesIcon = (props: IconProps) => <Icon type="global" {...props}/>;
 export const RecordsIcon = (props: IconProps) => <Icon type="read" {...props}/>;
+export const ArchiveIcon = (props: IconProps) => <Icon type="export" {...props}/>;
 
 export const FavouritesIcon = (props: IconProps) => <Icon type="star" {...props}/>;
 export const SearchIcon = (props: IconProps) => <Icon type="search" {...props}/>;
@@ -21,6 +26,11 @@ export const FilterIcon = (props: IconProps) => <Icon type="filter" {...props}/>
 export const ImageIcon = (props: IconProps) => <Icon type="file-image" {...props}/>;
 export const StatisticsIcon = (props: IconProps) => <Icon type="experiment" {...props}/>;
 
+export const DeleteIcon = (props: IconProps) => <Icon type="delete" {...props}/>;
+export const MergeIcon = (props: IconProps) => <Icon type="swap" {...props}/>;
+
 export const PrevIcon = (props: IconProps) => <Icon type="left-square" {...props}/>;
 export const NextIcon = (props: IconProps) => <Icon type="right-square" {...props}/>;
 export const EditIcon = (props: IconProps) => <Icon type="edit" {...props}/>;
+
+export const LinkIcon = (props: IconProps) => <Icon type="link" {...props}/>;
