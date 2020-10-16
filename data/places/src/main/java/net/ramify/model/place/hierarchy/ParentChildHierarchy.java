b412806b9ed5a2139ac8a13c1c267f18cee1724a
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2020-10-16 21:36:56 +0100

    Place hierarchies

--- data/places/src/main/java/net/ramify/model/place/hierarchy/ParentChildHierarchy.java
+++ data/places/src/main/java/net/ramify/model/place/hierarchy/ParentChildHierarchy.java
@@ -61,7 +61,7 @@ public class ParentChildHierarchy implements PlaceHierarchy {
 
     @Override
     public String toString() {
-        return parent + "->" + child;
+        return parent + "<-" + child;
     }
 
 }
