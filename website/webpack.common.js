58ed5ecd4182607327e4091075a01ce936760ba8
Ollie Robertshaw <oliver.robertshaw@mahifx.com>
2020-11-08 20:08:27 +0000

    Multiple vendor chunks

--- website/webpack.common.js
+++ website/webpack.common.js
@@ -25,14 +25,11 @@ module.exports = {
             chunks: "all",
             maxInitialRequests: Infinity,
             minSize: 0,
+            maxSize: 1000000,
             cacheGroups: {
                 vendor: {
                     name: "vendors",
-                    test: /node_modules/
-                    // name(module) {
-                    //     const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
-                    //     return `npm.${packageName.replace('@', '')}`;
-                    // }
+                    test: /node_modules/,
                 }
             }
         }
